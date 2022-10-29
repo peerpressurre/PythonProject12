8413cee92649b52d029c8008e82c0aa0b988b10f
“Your <yaroslavdisiak@gmail.com>
2022-10-29 13:01:16 +0300

    Update:feature/lab_ex2

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-#feature/lab_ex1 rstring = str[::-1]
-def rev():
+#feature/lab_ex1
+def con():
     try:
-        str = input('enter string->')
-
-        print(str[::-1])
+        text = input('enter text->')
+        counter = 0
+        counter2 = 0
+        for i in text:
+            if i.isalpha():
+                counter += 1
+            if i.isnumeric():
+                counter2 += 1
+        print(f'Number of letters: {counter}\nNumber of digits: {counter2} ')
     except Exception as ex:
         print(f'Error: {ex}')
-print(rev()
+
+con()
+
+
+
